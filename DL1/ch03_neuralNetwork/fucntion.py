6b9dcebde11e06b8c13b871a70fbc7faa174b553
gahyun01 <gahyun727301@kangnam.ac.kr>
2025-02-06 18:10:56 +0900

    .

--- DL1/ch03_neuralNetwork/fucntion.py
+++ DL1/ch03_neuralNetwork/fucntion.py
@@ -3,3 +3,11 @@ import numpy as np
 # 시그모이드 함수
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+# 소프트맥스 함수
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c) # 오버플로 대책
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
